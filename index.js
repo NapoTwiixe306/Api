ca5d406b51caf3196abb261adf5b0422c09cab9c
Julien Milants <julien.milants@gmail.com>
2021-10-18 18:40:31 +0200

    continue

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
 require('./models/dbConfig');
+const postsRoutes = require('./routes/postsController');
+
+
 
 app.listen(5500, () => console.log('Server started: 5500'))
